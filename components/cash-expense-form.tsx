aef2dc833637e64b13a5fde56815c5d5f8199269
ldecima-web <l.decima@lebane.app>
2026-05-03 20:10:02 -0300

    Commit

--- components/cash-expense-form.tsx
+++ components/cash-expense-form.tsx
@@ -31,10 +31,7 @@ function formatDateForInput(date: Date): string {
 }
 
 function formatDateDisplay(dateString: string): string {
-  const date = new Date(dateString)
-  const day = String(date.getDate()).padStart(2, '0')
-  const month = String(date.getMonth() + 1).padStart(2, '0')
-  const year = date.getFullYear()
+  const [year, month, day] = dateString.split('-')
   return `${day}/${month}/${year}`
 }
 
